d2c1c72ac6b01d706433e4f991a6e428dd56fc47
Paresh Patil <patilparesh20597@gmail.com>
2023-08-18 01:08:47 +0530

    Frontend Login and Signin pages ERROR

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Login from './Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signup from './Signup'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}></Route>
+        <Route path='/signup' element={<Signup />}></Route>
+      </Routes>
+    </BrowserRouter>
+
+
+  )
+}
+
+export default App
